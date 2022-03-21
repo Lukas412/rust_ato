05e26d165dab5c1f6a8f94ef2c4a79e06f96ff5c
lukas <lukas.weber.4.12@gmail.com>
2022-03-21 22:55:09 +0100

    remove logic

--- src/core/element/boolean/parameter.rs
+++ src/core/element/boolean/parameter.rs
@@ -1,7 +1,6 @@
 use std::iter;
 
 use crate::core::namespace::compare_namespace;
-use crate::core::requirement::{Container, Requirement, Value};
 
 #[derive(Debug, YaDeserialize)]
 #[yaserde(rename = "parameter", prefix = "boolean", namespace = "boolean: http://www.ato.net/xmlns/element/boolean")]
@@ -10,15 +9,4 @@ pub struct BooleanParameter {
   name: String,
   #[yaserde(attribute)]
   namespace: Option<String>,
-}
-
-impl Requirement for BooleanParameter {
-  fn satisfied_by(&self, container: Container) -> bool {
-    if self.namespace.is_none() {
-      return true;
-    }
-    let container_values = container.get_values().iter().map(Value::get_namespace);
-    let namespace = iter::repeat(self.namespace.as_ref().unwrap());
-    iter::zip(container_values, namespace).any(compare_namespace)
-  }
 }
