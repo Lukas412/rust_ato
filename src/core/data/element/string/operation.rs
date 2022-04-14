826b8238060a14a1e8303ab7a5db3af14874004d
lukas <lukas.weber.4.12@gmail.com>
2022-04-14 16:17:53 +0200

    implement build for string operationm

--- src/core/data/element/string/operation.rs
+++ src/core/data/element/string/operation.rs
@@ -1,6 +1,10 @@
 pub mod value;
 
+use crate::core::data::element::string::element::StringElement;
 use crate::core::data::element::string::operation::value::StringValueOperation;
+use crate::core::data::requirement::Requirements;
+use crate::core::traits::build::BuildableWithRequirements;
+use crate::core::traits::element::Element;
 
 #[derive(Debug, YaDeserialize)]
 #[yaserde(prefix = "string", namespace = "string: http://www.ato.net/xmlns/element/string")]
@@ -14,5 +18,14 @@ pub enum StringOperation {
 impl Default for StringOperation {
   fn default() -> Self {
     Self::Empty
+  }
+}
+
+impl BuildableWithRequirements<StringElement, String, Requirements> for StringOperation {
+  fn build_with_requirements(&self, requirements: Requirements) -> Result<StringElement, String> {
+    match self {
+      Self::Empty => Ok(StringElement::new("".to_string())),
+      Self::Value(operation) => operation.build_with_requirements(requirements)
+    }
   }
 }
