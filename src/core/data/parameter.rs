ca5c20bf31570df09a62213cc58c5a3ef63d03ce
lukas <lukas.weber.4.12@gmail.com>
2022-04-14 02:08:19 +0200

    add yaserde to parameters

--- src/core/data/parameter.rs
+++ src/core/data/parameter.rs
@@ -3,12 +3,14 @@ use crate::core::data::element::number::parameter::NumberParameter;
 use crate::core::data::element::path::parameter::PathParameter;
 use crate::core::data::element::string::parameter::StringParameter;
 
-pub struct Parameters(Vec<Parameter>);
-
 #[derive(Debug, YaDeserialize)]
-enum Parameter {
-  Boolean(BooleanParameter),
-  Number(NumberParameter),
-  Path(PathParameter),
-  String(StringParameter),
+pub struct Parameters {
+  #[yaserde(rename = "parameter", prefix = "boolean", namespace = "boolean: http://www.ato.net/xmlns/element/boolean")]
+  boolean: Vec<BooleanParameter>,
+  #[yaserde(rename = "parameter", prefix = "number", namespace = "number: http://www.ato.net/xmlns/element/number")]
+  number: Vec<NumberParameter>,
+  #[yaserde(rename = "parameter", prefix = "path", namespace = "path: http://www.ato.net/xmlns/element/path")]
+  path: Vec<PathParameter>,
+  #[yaserde(rename = "parameter", prefix = "string", namespace = "string: http://www.ato.net/xmlns/element/string")]
+  string: Vec<StringParameter>
 }
