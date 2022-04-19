7054b657b9f964763f4863d8da9f2f05654406a9
lukas <lukas.weber.4.12@gmail.com>
2022-04-19 22:48:55 +0200

    implement yaserde for general parameter

--- src/core/data/parameter.rs
+++ src/core/data/parameter.rs
@@ -1,8 +1,14 @@
 use std::io::Read;
+use yaserde::__xml::name::OwnedName;
 
 use yaserde::__xml::reader::XmlEvent;
 use yaserde::de::Deserializer;
 use yaserde::YaDeserialize;
+use crate::core::data::element::boolean::parameter::BooleanParameter;
+use crate::core::data::element::number::parameter::NumberParameter;
+use crate::core::data::element::path::parameter::PathParameter;
+use crate::core::data::element::string::parameter::StringParameter;
+use crate::core::parse::from_deserializer;
 
 #[derive(Debug, Default, YaDeserialize)]
 pub struct Parameters {
@@ -10,61 +16,37 @@ pub struct Parameters {
   parameters: Vec<Parameter>,
 }
 
-#[derive(Debug, Default)]
-pub struct Parameter {
-  name: String,
-  namespace: Option<String>,
-  parameter_type: ParameterType,
+#[derive(Debug)]
+pub enum Parameter {
+  Boolean(BooleanParameter),
+  Number(NumberParameter),
+  Path(PathParameter),
+  String(StringParameter),
 }
 
-impl YaDeserialize for Parameter {
-  fn deserialize<R: Read>(reader: &mut Deserializer<R>) -> Result<Self, String> {
-    let mut result = Parameter::default();
-    if let XmlEvent::StartElement { name, namespace, attributes } = reader.peek() {
-      if name.local_name != "parameter" {
-        return Err(format!("Expected name parameter, got {}", name.local_name));
+impl Parameter {
+  fn from_xml_name<R: Read>(reader: &mut Deserializer<R>, name: OwnedName) -> Result<Self, String> {
+    if let OwnedName { local_name, namespace: Some(namespace), .. } = name {
+      match (local_name.as_str(), namespace.as_str()) {
+        ("parameter", "http://www.ato.net/xmlns/element/boolean") => Ok(Parameter::Boolean(from_deserializer(reader)?)),
+        ("parameter", "http://www.ato.net/xmlns/element/number") => Ok(Parameter::Number(from_deserializer(reader)?)),
+        ("parameter", "http://www.ato.net/xmlns/element/path") => Ok(Parameter::Path(from_deserializer(reader)?)),
+        ("parameter", "http://www.ato.net/xmlns/element/string") => Ok(Parameter::String(from_deserializer(reader)?)),
+        value => Err(format!("parameter: wrong name: {:?}", value))
       }
-      result.parameter_type = ParameterType::from_namespace(&name.namespace)?;
-    };
-    reader.read_inner_value(|f| {
-      loop {
-        let peek = f.peek()?;
-        match peek {
-          XmlEvent::StartElement { .. } => {}
-          XmlEvent::EndElement { .. } => {}
-          XmlEvent::CData(_) => {}
-          XmlEvent::Characters(_) => {}
-          _ => {}
-        }
-        f.next_event();
-      }
-    })
-  }
-}
-
-pub enum ParameterType {
-  None,
-  Boolean,
-  Number,
-  Path,
-  String,
-}
-
-impl ParameterType {
-  fn from_namespace(namespace: &Option<String>) -> Result<Self, String> {
-    let actual_namespace = namespace.ok_or("parameter has to have a namespace".to_string())?;
-    match actual_namespace.as_str() {
-      "http://www.ato.net/xmlns/element/boolean" => Ok(Self::Boolean),
-      "http://www.ato.net/xmlns/element/number" => Ok(Self::Number),
-      "http://www.ato.net/xmlns/element/path" => Ok(Self::Path),
-      "http://www.ato.net/xmlns/element/string" => Ok(Self::String),
-      value => Err(format!("unknown namespace for parameter: {}", value))
+    } else {
+      Err("parameter: no namespace found".to_string())
     }
   }
 }
 
-impl Default for ParameterType {
-  fn default() -> Self {
-    Self::None
+impl YaDeserialize for Parameter {
+  fn deserialize<R: Read>(reader: &mut Deserializer<R>) -> Result<Self, String> {
+    let peek = reader.peek()?.to_owned();
+    if let XmlEvent::StartElement { name, .. } = peek {
+      Parameter::from_xml_name(reader, name)
+    } else {
+      Err(format!("parameter: wrong xml format: {:?}", peek))
+    }
   }
 }
