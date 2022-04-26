dbf5c69827e3ebbadc80d2d585344ea727ae3979
lukas <lukas.weber.4.12@gmail.com>
2022-04-26 19:44:26 +0200

    add ContainerValue

--- src/core/traits/container.rs
+++ src/core/traits/container.rs
@@ -1,4 +1,10 @@
-pub trait Container<Value> {
+pub trait Container<Value: ContainerValue<T>, T> {
   fn new(values: Vec<Value>) -> Self;
   fn value(&self, namespace: String, name: String) -> Value;
+}
+
+pub trait ContainerValue<T> {
+  fn value(&self) -> T;
+  fn name(&self) -> String;
+  fn namespace(&self) -> String;
 }
