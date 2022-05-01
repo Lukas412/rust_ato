02870e12c43834cb4ec879ae91062bfeb481e9d7
lukas <lukas.weber.4.12@gmail.com>
2022-05-01 23:02:44 +0200

    update container trait

--- src/core/traits/container.rs
+++ src/core/traits/container.rs
@@ -1,7 +1,8 @@
+use crate::core::data::name::AtoName;
 use crate::core::traits::element::Element;
+use crate::core::traits::parameter::Parameter;
 
 pub trait Container<E: Element<T>, T> {
-  fn element(&self) -> &E;
-  fn name(&self) -> &String;
-  fn namespace(&self) -> &String;
+  fn has_parameter<P: Parameter>(&self, parameters: &P) -> bool;
+  fn get_element(&self, name: &AtoName) -> &E;
 }
