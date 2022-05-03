ef7747750aedea24502c929fd3553eade8a4d7c6
lukas <lukas.weber.4.12@gmail.com>
2022-05-03 20:50:26 +0200

    impl Value for GeneralValue

--- src/core/data/general/value.rs
+++ src/core/data/general/value.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+use rust_decimal::Decimal;
 use crate::core::data::boolean::value::BooleanValue;
 use crate::core::data::number::value::NumberValue;
 use crate::core::data::path::value::PathValue;
@@ -9,4 +11,42 @@ pub enum GeneralValue {
   Number(NumberValue),
   Path(PathValue),
   String(StringValue),
+}
+
+pub enum CombinedValue {
+  Boolean(bool),
+  Number(Decimal),
+  Path(PathBuf),
+  String(String),
+}
+
+impl Value for GeneralValue {
+  type Type = CombinedValue;
+
+  fn new(value: Self::Type, namespace: String) -> Self {
+    match value {
+      CombinedValue::Boolean(value) => Self::Boolean(BooleanValue::new(value, namespace)),
+      CombinedValue::Number(value) => Self::Number(NumberValue::new(value, namespace)),
+      CombinedValue::Path(value) => Self::Path(PathValue::new(value, namespace)),
+      CombinedValue::String(value) => Self::String(StringValue::new(value, namespace)),
+    }
+  }
+
+  fn value(&self) -> &Self::Type {
+    match &self {
+      Self::Boolean(value) => &CombinedValue::Boolean(*value.value()),
+      Self::Number(value) => &CombinedValue::Number(*value.value()),
+      Self::Path(value) => &CombinedValue::Path(value.value().to_owned()),
+      Self::String(value) => &CombinedValue::String(value.value().to_owned()),
+    }
+  }
+
+  fn namespace(&self) -> &String {
+    match &self {
+      Self::Boolean(value) => value.namespace(),
+      Self::Number(value) => value.namespace(),
+      Self::Path(value) => value.namespace(),
+      Self::String(value) => value.namespace(),
+    }
+  }
 }
