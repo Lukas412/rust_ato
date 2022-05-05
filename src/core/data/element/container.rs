a7cbb8470151e74cb4b7adaf44eab6fb0918eb8b
lukas <lukas.weber.4.12@gmail.com>
2022-05-05 19:41:02 +0200

    add element container

---
+++ src/core/data/element/container.rs
@@ -0,0 +1,17 @@
+use crate::core::data::element::parameter::ElementParameter;
+use crate::core::data::element::value::ElementValue;
+use crate::core::traits::container::Container;
+
+pub struct ElementContainer {
+
+}
+
+impl Container<ElementValue, ElementParameter> for ElementContainer {
+  fn satisfy_parameter(&self, parameters: &ElementParameter) -> bool {
+    todo!()
+  }
+
+  fn get_element(&self, name: &String, namespace: Option<String>) -> &ElementValue {
+    todo!()
+  }
+}
