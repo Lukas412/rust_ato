da21eab1721545bc9cee856fabce060f867410bc
lukas <lukas.weber.4.12@gmail.com>
2022-05-06 19:35:41 +0200

    add bool and decimal to ElementContainer

--- src/core/data/element/container.rs
+++ src/core/data/element/container.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
+use rust_decimal::Decimal;
+use crate::core::data::action::value::Action;
+use crate::core::data::build::{BuildError, RequirementError};
 use crate::core::data::element::parameter::ElementParameter;
 use crate::core::data::element::value::ElementValue;
-use crate::core::traits::container::Container;
+use crate::core::traits::container::{Container, Provide};
 use crate::core::traits::parameter::Parameter;
+use crate::core::traits::value::Value;
 
 pub struct ElementContainer {
   elements: HashMap<String, ElementValue>
@@ -26,5 +30,27 @@ impl Container for ElementContainer {
 
   fn get_element(&self, name: &String) -> Option<&Self::Value> {
     self.elements.get(name)
+  }
+}
+
+impl Provide<bool> for ElementContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&bool, Self::Error> {
+    match self.get_element(name) {
+      Some(ElementValue::Boolean(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
+  }
+}
+
+impl Provide<Decimal> for ElementContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&Decimal, Self::Error> {
+    match self.get_element(name) {
+      Some(ElementValue::Number(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
   }
 }
