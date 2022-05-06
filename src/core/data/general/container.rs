ec79252474c4028d3d8c10e139aad8ff87a1e5b3
lukas <lukas.weber.4.12@gmail.com>
2022-05-06 20:08:27 +0200

    add provide all values in general container

--- src/core/data/general/container.rs
+++ src/core/data/general/container.rs
@@ -1,9 +1,14 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
+use rust_decimal::Decimal;
+use crate::core::data::action::value::Action;
+use crate::core::data::build::{BuildError, RequirementError};
 
 use crate::core::data::general::parameter::GeneralParameter;
 use crate::core::data::general::value::GeneralValue;
-use crate::core::traits::container::Container;
+use crate::core::traits::container::{Container, Provide};
 use crate::core::traits::parameter::Parameter;
+use crate::core::traits::value::Value;
 
 pub struct GeneralContainer {
   elements: HashMap<String, GeneralValue>,
@@ -27,5 +32,60 @@ impl Container for GeneralContainer {
 
   fn get_element(&self, name: &String) -> Option<&Self::Value> {
     self.elements.get(name)
+  }
+}
+
+impl Provide<Action> for GeneralContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&Action, Self::Error> {
+    match self.get_element(name) {
+      Some(GeneralValue::Action(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
+  }
+}
+
+impl Provide<bool> for GeneralContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&bool, Self::Error> {
+    match self.get_element(name) {
+      Some(GeneralValue::Boolean(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
+  }
+}
+
+impl Provide<Decimal> for GeneralContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&Decimal, Self::Error> {
+    match self.get_element(name) {
+      Some(GeneralValue::Number(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
+  }
+}
+
+impl Provide<PathBuf> for GeneralContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&PathBuf, Self::Error> {
+    match self.get_element(name) {
+      Some(GeneralValue::Path(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
+  }
+}
+
+impl Provide<String> for GeneralContainer {
+  type Error = BuildError;
+
+  fn get_value(&self, name: &String, namespace: &String) -> Result<&String, Self::Error> {
+    match self.get_element(name) {
+      Some(GeneralValue::String(value)) => Ok(value.value()),
+      _ => Err(RequirementError::new(name, namespace)),
+    }
   }
 }
