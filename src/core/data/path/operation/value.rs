c97ed0d4d770c1a473969b22931f7467dd39fef7
lukas <lukas.weber.4.12@gmail.com>
2022-05-07 23:20:39 +0200

    fix build for PathValueOperation

--- src/core/data/path/operation/value.rs
+++ src/core/data/path/operation/value.rs
@@ -1,7 +1,9 @@
+use std::path::PathBuf;
 use crate::core::data::build::BuildError;
 use crate::core::data::path::value::PathValue;
 use crate::core::traits::build::Buildable;
 use crate::core::traits::container::Provide;
+use crate::core::traits::value::Value;
 
 #[derive(Debug, YaDeserialize)]
 #[yaserde(rename = "value", prefix = "path", namespace = "path: http://www.ato.net/xmlns/element/path")]
@@ -11,7 +13,9 @@ pub struct PathValueOperation {
 }
 
 impl<C: Provide<PathValue>> Buildable<PathValue, BuildError, C> for PathValueOperation {
-  fn build(&self, _: &C) -> Result<PathValue, BuildError> {
-    PathValue::from_str(&self.text)
+  fn build(&self, requirements: &C) -> Result<PathValue, BuildError> {
+    let value = PathBuf::from(&self.text);
+    let namespace = requirements.namespace().to_owned();
+    Ok(PathValue::new(value, namespace))
   }
 }
