f26aae8061068cc23fc8099877c61247099d43e8
lukas <lukas.weber.4.12@gmail.com>
2022-05-07 18:52:32 +0200

    remove ValueFromString

--- src/core/traits/value.rs
+++ src/core/traits/value.rs
@@ -1,16 +1,9 @@
+use crate::core::data::build::ValueError;
+
 pub trait Value {
   type Type;
 
   fn new(value: Self::Type, namespace: String) -> Self;
   fn value(&self) -> &Self::Type;
   fn namespace(&self) -> &String;
-}
-
-pub trait ValueFromString: Value {
-  fn from_string(string: &str, namespace: &String) -> Box<Self> {
-    let value = Self::value_from_string(string);
-    Self::new(value, namespace.to_owned())
-  }
-
-  fn value_from_string(string: &str) -> Self::Type;
 }
