2794e00e7ca9e4f4bee645d0ce05621666903525
lukas <lukas.weber.4.12@gmail.com>
2022-05-07 18:33:09 +0200

    add ValueFromString

--- src/core/traits/value.rs
+++ src/core/traits/value.rs
@@ -5,3 +5,12 @@ pub trait Value {
   fn value(&self) -> &Self::Type;
   fn namespace(&self) -> &String;
 }
+
+pub trait ValueFromString: Value {
+  fn from_string(string: &str, namespace: &String) -> Box<Self> {
+    let value = Self::value_from_string(string);
+    Self::new(value, namespace.to_owned())
+  }
+
+  fn value_from_string(string: &str) -> Self::Type;
+}
