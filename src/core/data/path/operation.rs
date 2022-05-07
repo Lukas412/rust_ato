fc2a22b89758c1b10b95856a1ff35424b2b0e87a
lukas <lukas.weber.4.12@gmail.com>
2022-05-07 23:21:04 +0200

    fix build for PathOperation

--- src/core/data/path/operation.rs
+++ src/core/data/path/operation.rs
@@ -1,11 +1,13 @@
 mod value;
 
+use std::path::PathBuf;
 use crate::Container;
 use crate::core::data::build::BuildError;
 use crate::core::data::path::value::PathValue;
 use crate::core::data::path::operation::value::PathValueOperation;
 use crate::core::traits::build::Buildable;
 use crate::core::traits::container::Provide;
+use crate::core::traits::value::Value;
 
 #[derive(Debug, YaDeserialize)]
 #[yaserde(prefix = "path", namespace = "path: http://www.ato.net/xmlns/element/path")]
@@ -25,7 +27,7 @@ impl Default for PathOperation {
 impl<C: Container + Provide<PathValue>> Buildable<PathValue, BuildError, C> for PathOperation {
   fn build(&self, requirements: &C) -> Result<PathValue, BuildError> {
     match self {
-      Self::Empty => Ok(PathValue::from_str("")),
+      Self::Empty => Ok(PathValue::new(PathBuf::default(), requirements.namespace().to_owned())),
       Self::Value(operation) => operation.build(requirements),
     }
   }
