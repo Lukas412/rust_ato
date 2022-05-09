efddb32e9099c1a0bd57fcd18cb745b64c8ec413
lukas <lukas.weber.4.12@gmail.com>
2022-05-09 19:30:10 +0200

    add yaserde to creation

--- src/core/main/creation.rs
+++ src/core/main/creation.rs
@@ -1,19 +1,33 @@
+#[derive(Debug, YaDeserialize)]
+#[yaserde(rename = "main", prefix = "creation", namepsace = "creation: http://www.ato.net/xmlns/creation")]
 pub struct Creation {
   uses: CreationUses,
+  #[yaserde(prefix = "creation", namepsace = "creation: http://www.ato.net/xmlns/creation")]
   element: CreationElement,
 }
 
+#[derive(Debug, YaDeserialize)]
 pub struct CreationUses {
+  #[yaserde(rename = "use")]
   uses: Vec<String>,
 }
 
+#[derive(Debug, YaDeserialize)]
+#[yaserde(rename = "element", prefix = "creation", namepsace = "creation: http://www.ato.net/xmlns/creation")]
 pub struct CreationElement {
+  #[yaserde(attribute)]
   namespace: String,
-  values: CreationValue,
+  #[yaserde(rename = "value")]
+  values: Vec<CreationValue>,
 }
 
+#[derive(Debug, YaDeserialize)]
+#[yaserde(rename = "value")]
 pub struct CreationValue {
+  #[yaserde(attribute)]
   name: String,
+  #[yaserde(attribute)]
   value: Option<String>,
+  #[yaserde(rename = "element", prefix = "creation", namepsace = "creation: http://www.ato.net/xmlns/creation")]
   elements: Vec<CreationElement>,
 }
