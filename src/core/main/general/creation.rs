8c29ec2cdcad3e17a18641704b9951d9c355f788
lukas <lukas.weber.4.12@gmail.com>
2022-05-10 10:20:42 +0200

    remove uses from creation

--- src/core/main/general/creation.rs
+++ src/core/main/general/creation.rs
@@ -1,26 +1,5 @@
 use crate::core::main::general::value::GeneralValue;
 
-#[derive(Debug, YaDeserialize)]
-#[yaserde(root, rename = "main", prefix = "creation", namespace = "creation: http://www.ato.net/xmlns/creation")]
-pub struct GeneralCreation {
-  uses: GeneralCreationUses,
-  #[yaserde(prefix = "creation", namespace = "creation: http://www.ato.net/xmlns/creation")]
-  element: GeneralCreationElement,
-}
-
-impl GeneralCreation {
-  fn build(&self) -> GeneralValue {
-    self.element.build()
-  }
-}
-
-#[derive(Debug, Default, YaDeserialize)]
-#[yaserde(rename = "uses")]
-pub struct GeneralCreationUses {
-  #[yaserde(rename = "use")]
-  uses: Vec<String>,
-}
-
 #[derive(Debug, Default, YaDeserialize)]
 #[yaserde(rename = "element", prefix = "creation", namespace = "creation: http://www.ato.net/xmlns/creation")]
 pub struct GeneralCreationElement {
