f2284448d2572a1a12e7224bc0919ca79e907eaf
lukas <lukas.weber.4.12@gmail.com>
2022-05-12 19:59:11 +0200

    update type annotations

--- src/core/main/string/operation.rs
+++ src/core/main/string/operation.rs
@@ -26,7 +26,10 @@ impl Default for StringOperation {
   }
 }
 
-impl<C: Container + Provide<StringValue, BuildError>> Buildable<StringValue, BuildError, C> for StringOperation {
+impl<C> Buildable<StringValue, BuildError, C> for StringOperation
+  where
+    C: Container + Provide<StringValue, BuildError>
+{
   fn build(&self, requirements: &C) -> Result<StringValue, BuildError> {
     match self {
       Self::Empty => Ok(StringValue::new("".to_owned(), requirements.namespace().to_owned())),
