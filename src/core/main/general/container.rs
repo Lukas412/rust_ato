b0703c6a7b84e8df28a698919114fd1390d5c82f
lukas <lukas.weber.4.12@gmail.com>
2022-05-15 01:10:48 +0200

    provide operation from container

--- src/core/main/general/container.rs
+++ src/core/main/general/container.rs
@@ -2,38 +2,37 @@ use std::collections::HashMap;
 use std::iter::FromIterator;
 use crate::core::build::error::BuildError;
 use crate::core::main::action::value::ActionValue;
+use crate::core::main::boolean::operation::BooleanOperation;
 use crate::core::main::boolean::value::BooleanValue;
+use crate::core::main::general::operation::CombinedGeneralOperation;
 
 use crate::core::main::general::parameter::GeneralParameter;
 use crate::core::main::general::value::{CombinedGeneralValue, GeneralValue};
+use crate::core::main::number::operation::NumberOperation;
 use crate::core::main::number::value::NumberValue;
+use crate::core::main::path::operation::PathOperation;
 use crate::core::main::path::value::PathValue;
+use crate::core::main::string::operation::StringOperation;
 use crate::core::main::string::value::StringValue;
 use crate::core::traits::container::Container;
+use crate::core::traits::operation::ProvideOperation;
 use crate::core::traits::pack::Pack;
 use crate::core::traits::parameter::Parameter;
 use crate::core::traits::value::Value;
 
 pub struct GeneralContainer {
   namespace: String,
-  elements: HashMap<String, GeneralValue>,
-}
-
-impl GeneralContainer {
-  fn get_value_and_namespace(&self, name: &String, namespace: &String) -> Result<(&CombinedGeneralValue, String), BuildError> {
-    match self.get_element(name) {
-      Some(value) => Ok((value.value(), value.namespace().to_owned())),
-      _ => Err(BuildError::new_requirement(name.to_owned(), namespace.to_owned())),
-    }
-  }
+  elements: HashMap<String, CombinedGeneralOperation>,
 }
 
 impl Container for GeneralContainer {
-  type Operation = ();
+  type Operation = CombinedGeneralOperation;
   type Parameter = GeneralParameter;
 
   fn from_pack<P: Pack>(pack: P, elements: Vec<(String, Self::Operation)>) -> Self {
-    todo!()
+    let namespace = pack.namespace().to_owned();
+    let elements = HashMap::from_iter(elements.into_iter());
+    Self { namespace, elements }
   }
 
   fn namespace(&self) -> &String {
@@ -44,52 +43,43 @@ impl Container for GeneralContainer {
     self.elements.contains_key(parameter.name())
   }
 
-  fn get_element(&self, name: &String) -> Option<&Self::Value> {
+  fn get_element(&self, name: &String) -> Option<&Self::Operation> {
     self.elements.get(name)
   }
 }
 
-impl Provide<ActionValue> for GeneralContainer {
-  fn get(&self, name: &String, namespace: &String) -> Result<ActionValue, BuildError> {
-    match self.get_value_and_namespace(name, namespace)? {
-      (CombinedGeneralValue::Action(value), namespace) => Ok(ActionValue::new(value.to_owned(), namespace)),
-      _ => Err(BuildError::new_requirement(name.to_owned(), namespace.to_owned())),
+impl ProvideOperation<BooleanOperation> for GeneralContainer {
+  fn operation(&self, name: &String) -> Option<&BooleanOperation> {
+    match self.get_element(name) {
+      Some(CombinedGeneralOperation::Boolean(&operation)) => Some(operation),
+      _ => None,
     }
   }
 }
 
-impl Provide<BooleanValue> for GeneralContainer {
-  fn get(&self, name: &String, namespace: &String) -> Result<BooleanValue, BuildError> {
-    match self.get_value_and_namespace(name, namespace)? {
-      (CombinedGeneralValue::Boolean(value), namespace) => Ok(BooleanValue::new(value.to_owned(), namespace)),
-      _ => Err(BuildError::new_requirement(name.to_owned(), namespace.to_owned())),
+impl ProvideOperation<NumberOperation> for GeneralContainer {
+  fn operation(&self, name: &String) -> Option<&NumberOperation> {
+    match self.get_element(name) {
+      Some(CombinedGeneralOperation::Number(&operation)) => Some(operation),
+      _ => None,
     }
   }
 }
 
-impl Provide<NumberValue> for GeneralContainer {
-  fn get(&self, name: &String, namespace: &String) -> Result<NumberValue, BuildError> {
-    match self.get_value_and_namespace(name, namespace)? {
-      (CombinedGeneralValue::Number(value), namespace) => Ok(NumberValue::new(value.to_owned(), namespace)),
-      _ => Err(BuildError::new_requirement(name.to_owned(), namespace.to_owned())),
+impl ProvideOperation<PathOperation> for GeneralContainer {
+  fn operation(&self, name: &String) -> Option<&PathOperation> {
+    match self.get_element(name) {
+      Some(CombinedGeneralOperation::Path(&operation)) => Some(operation),
+      _ => None,
     }
   }
 }
 
-impl Provide<PathValue> for GeneralContainer {
-  fn get(&self, name: &String, namespace: &String) -> Result<PathValue, BuildError> {
-    match self.get_value_and_namespace(name, namespace)? {
-      (CombinedGeneralValue::Path(value), namespace) => Ok(PathValue::new(value.to_owned(), namespace)),
-      _ => Err(BuildError::new_requirement(name.to_owned(), namespace.to_owned())),
-    }
-  }
-}
-
-impl Provide<StringValue> for GeneralContainer {
-  fn get(&self, name: &String, namespace: &String) -> Result<StringValue, BuildError> {
-    match self.get_value_and_namespace(name, namespace)? {
-      (CombinedGeneralValue::String(value), namespace) => Ok(StringValue::new(value.to_owned(), namespace)),
-      _ => Err(BuildError::new_requirement(name.to_owned(), namespace.to_owned())),
+impl ProvideOperation<StringOperation> for GeneralContainer {
+  fn operation(&self, name: &String) -> Option<&StringOperation> {
+    match self.get_element(name) {
+      Some(CombinedGeneralOperation::String(&operation)) => Some(operation),
+      _ => None,
     }
   }
 }
