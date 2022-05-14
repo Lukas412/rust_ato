2bd0bce3aed2757f9c368eabf32be4b85fed9fda
lukas <lukas.weber.4.12@gmail.com>
2022-05-14 16:19:11 +0200

    use from_file method

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs::read_to_string;
 
 use yaserde::de::from_str;
 use crate::core::main::general::pack::PackProvider;
+use crate::core::parse::from_file;
 use crate::core::traits::build::Buildable;
 
 
@@ -14,8 +15,7 @@ mod core;
 
 fn main() {
   let pack_provider = PackProvider::from_root("src/bundles".as_ref());
-  let string = read_to_string("src/creations/empty-git.creation.xml").unwrap();
-  let creation: GeneralCreation = from_str(&string).unwrap();
+  let creation: GeneralCreation = from_file("src/creations/empty-git.creation.xml").unwrap();
   let value = creation.build(&pack_provider).unwrap();
   println!("{:?}", value);
 }
