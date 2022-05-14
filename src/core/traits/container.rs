8c69b6a0f2b0e6a390cd01002cc5a1ee65391f33
lukas <lukas.weber.4.12@gmail.com>
2022-05-14 20:14:12 +0200

    update Container

--- src/core/traits/container.rs
+++ src/core/traits/container.rs
@@ -1,18 +1,15 @@
 use crate::core::build::error::BuildError;
+use crate::core::traits::operation::Operation;
 use crate::core::traits::pack::Pack;
 use crate::core::traits::value::Value;
 use crate::core::traits::parameter::Parameter;
 
 pub trait Container {
-  type Value: Value;
+  type Operation: Operation;
   type Parameter: Parameter;
 
-  fn from_pack<P: Pack>(pack: P, elements: Vec<(String, Self::Value)>) -> Self;
+  fn from_pack<P: Pack>(pack: P, elements: Vec<(String, Self::Operation)>) -> Self;
   fn namespace(&self) -> &String;
   fn includes(&self, parameter: &Self::Parameter) -> bool;
-  fn get_element(&self, name: &String) -> Option<&Self::Value>;
-}
-
-pub trait Provide<T>: Container {
-  fn get(&self, name: &String, namespace: &String) -> Result<T, BuildError>;
+  fn get_element(&self, name: &String) -> Option<&Self::Operation>;
 }
