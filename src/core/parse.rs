a4b28f782bd50901c8b8dd6df764e82c42de2924
lukas <lukas.weber.4.12@gmail.com>
2022-05-14 16:17:11 +0200

    add from_file function

--- src/core/parse.rs
+++ src/core/parse.rs
@@ -1,7 +1,16 @@
+use std::fs::{File, read_to_string};
+use std::io;
 use std::io::Read;
 
-use yaserde::de::Deserializer;
+use yaserde::de::{Deserializer, from_reader, from_str};
 use yaserde::YaDeserialize;
+
+pub fn from_file<T: YaDeserialize>(file: &str) -> Result<T, String> {
+  match File::open(file) {
+    Ok(file) => from_reader(file),
+    Err(error) => Err(error.to_string()),
+  }
+}
 
 pub fn from_deserializer<R: Read, T: YaDeserialize>(reader: &mut Deserializer<R>) -> Result<T, String> {
   <T as YaDeserialize>::deserialize(reader)
