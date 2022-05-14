26c69634bb51f2ddaa343ffb4f9e6ca04b4c52c2
lukas <lukas.weber.4.12@gmail.com>
2022-05-15 00:57:42 +0200

    add combined operation

--- src/core/main/general/operation.rs
+++ src/core/main/general/operation.rs
@@ -1,1 +1,14 @@
+use crate::core::main::boolean::operation::BooleanOperation;
+use crate::core::main::number::operation::NumberOperation;
+use crate::core::main::path::operation::PathOperation;
+use crate::core::main::string::operation::StringOperation;
+
 pub mod empty;
+
+pub enum CombinedGeneralOperation {
+  Action,
+  Boolean(BooleanOperation),
+  Number(NumberOperation),
+  Path(PathOperation),
+  String(StringOperation),
+}
