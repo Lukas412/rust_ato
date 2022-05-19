3dec6d9e79a919e6b2a3b80674e2fdca0a27a093
lukas <lukas.weber.4.12@gmail.com>
2022-05-19 23:30:55 +0200

    add Buildable

--- src/core/traits/build.rs
+++ src/core/traits/build.rs
@@ -2,6 +2,12 @@ use crate::core::build::error::BuildError;
 use crate::core::main::general::requirements::RequirementBox;
 use crate::Requirements;
 
+pub trait Buildable<B> {
+  fn to_requirement_box(self) -> RequirementBox;
+
+  fn build(&self, requirements: &Requirements) -> Result<B, BuildError>;
+}
+
 pub trait BuildableWithRequirements<B> {
   fn to_requirement_box(self) -> RequirementBox;
 
