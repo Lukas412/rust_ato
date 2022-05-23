dfddda27586e9e926aadb2993a9039b37e7b51c9
lukas <lukas.weber.4.12@gmail.com>
2022-05-23 07:51:13 +0200

    remove requirements from buildable

--- src/core/traits/build.rs
+++ src/core/traits/build.rs
@@ -3,8 +3,6 @@ use crate::core::main::general::requirements::RequirementBox;
 use crate::Requirements;
 
 pub trait Buildable<B> {
-  fn to_requirement_box(self) -> RequirementBox;
-
   fn build(&self, requirements: &Requirements) -> Result<B, BuildError>;
 }
 
