8b2ad866c4128854689ca98c52b2c004cfb830a1
lukas <lukas.weber.4.12@gmail.com>
2022-05-29 16:25:26 +0200

    update operation trait

--- src/core/traits/build.rs
+++ src/core/traits/build.rs
@@ -2,10 +2,6 @@ use crate::core::build::error::BuildError;
 use crate::core::main::general::requirements::RequirementBox;
 use crate::{PackProvider, Requirements};
 
-pub trait Buildable<B> {
-  fn build(&self, pack_provider: &PackProvider, requirements: &mut Requirements) -> Result<B, BuildError>;
-}
-
 pub trait BuildableWithRequirements<B> {
   fn to_requirement_box(self) -> RequirementBox;
 
--- src/core/traits/operation.rs
+++ src/core/traits/operation.rs
@@ -1,17 +1,22 @@
-use crate::BuildError;
+use crate::{BuildError, PackProvider, Requirements};
+use crate::core::traits::value::Value;
 
-pub trait Operation {
-  type Value;
+pub trait Operation<V>
+  where V: Value
+{
+  fn build(&self, pack_provider: &PackProvider, requirements: &mut Requirements) -> Result<B, BuildError>;
 }
 
 pub trait ProvideOperation<O>
-  where O: Operation
+  where O: Operation<Self::Value>
 {
+  type Value: Value;
   fn operation(&self, name: &String) -> Result<&O, BuildError>;
 }
 
 pub trait ProvideOperationWithNamespace<O>
-  where O: Operation
+  where O: Operation<Self::Value>
 {
+  type Value: Value;
   fn operation(&self, namespace: &String, name: &String) -> Result<O, BuildError>;
 }
