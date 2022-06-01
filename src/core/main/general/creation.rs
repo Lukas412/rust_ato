063e12e67f1c88dcf861d60f12e622eea7ffcfcb
lukas <lukas.weber.4.12@gmail.com>
2022-06-01 22:56:38 +0200

    move creation value

--- src/core/main/general/creation.rs
+++ src/core/main/general/creation.rs
@@ -1,3 +1,5 @@
+mod value;
+
 use std::collections::HashMap;
 use std::io::Read;
 use std::iter::FromIterator;
@@ -5,6 +7,7 @@ use std::rc::Rc;
 
 use yaserde::de::Deserializer;
 use yaserde::{YaDeserialize, YaSerialize};
+use value::GeneralCreationValue;
 
 use crate::{PackProvider, Requirements};
 use crate::core::build::error::BuildError;
@@ -13,7 +16,7 @@ use crate::core::main::general::requirements::RequirementBox;
 use crate::core::main::string::pack::StringPack;
 use crate::core::main::string::value::StringValue;
 use crate::core::parse::from_deserializer;
-use crate::core::traits::build::{BuildableWithRequirements};
+use crate::core::traits::build::BuildableWithRequirements;
 use crate::core::traits::namespace::Namespace;
 use crate::core::traits::operation::Operation;
 use crate::core::traits::pack::{Pack, ProvidePack};
@@ -43,20 +46,11 @@ impl YaDeserialize for GeneralCreation {
 
 #[derive(Debug, Default, YaDeserialize)]
 #[yaserde(rename = "creation", prefix = "general", namespace = "general: http://www.ato.net/xmlns/general")]
-pub struct InnerGeneralCreation {
+struct InnerGeneralCreation {
   #[yaserde(attribute)]
   namespace: String,
   #[yaserde(rename = "value")]
   values: Vec<GeneralCreationValue>,
-}
-
-impl InnerGeneralCreation {
-  fn get_operation<'a, P, O: Operation<V>, V>(&self, pack_provider: &'a PackProvider) -> Result<&'a O, BuildError>
-    where PackProvider: ProvidePack<P, V>, P: 'a + Pack<V, Operation=O>, V: Value
-  {
-    let pack: &P = pack_provider.pack(&self.namespace)?;
-    Ok(pack.operation())
-  }
 }
 
 impl Buildable<StringValue> for InnerGeneralCreation {
@@ -65,47 +59,4 @@ impl Buildable<StringValue> for InnerGeneralCreation {
     let operation = pack.operation();
     operation.build(pack_provider, requirements)
   }
-}
-
-#[derive(Debug)]
-pub struct GeneralCreationValue {
-  name: String,
-  operation: GeneralOperation,
-}
-
-impl GeneralCreationValue {
-  fn new(name: String, operation: GeneralOperation) -> Self {
-    Self { name, operation }
-  }
-
-  pub fn to_name_and_operation(self) -> (String, Rc<GeneralOperation>) {
-    (self.name, Rc::new(self.operation))
-  }
-}
-
-impl YaDeserialize for GeneralCreationValue {
-  fn deserialize<R: Read>(reader: &mut Deserializer<R>) -> Result<Self, String> {
-    let inner: InnerGeneralCreationValue = from_deserializer(reader)?;
-    let (name, operation) =
-      match inner {
-        InnerGeneralCreationValue { name, value: Some(value), .. } =>
-          (name, GeneralOperation::Value(value)),
-        InnerGeneralCreationValue { name, elements, .. } if !elements.is_empty() =>
-          (name, GeneralOperation::Operation(elements)),
-        InnerGeneralCreationValue { name, .. } =>
-          (name, GeneralOperation::Empty)
-      };
-    Ok(Self::new(name, operation))
-  }
-}
-
-#[derive(Debug, YaDeserialize)]
-#[yaserde(rename = "value")]
-struct InnerGeneralCreationValue {
-  #[yaserde(attribute)]
-  name: String,
-  #[yaserde(attribute)]
-  value: Option<String>,
-  #[yaserde(rename = "creation", prefix = "general", namespace = "general: http://www.ato.net/xmlns/general")]
-  elements: Vec<InnerGeneralCreation>,
 }
---
+++ src/core/main/general/creation/value.rs
@@ -0,0 +1,50 @@
+use std::rc::Rc;
+use yaserde::YaDeserialize;
+use std::io::Read;
+use yaserde::de::Deserializer;
+use crate::core::main::general::creation::InnerGeneralCreation;
+use crate::core::main::general::operation::GeneralOperation;
+use crate::core::parse::from_deserializer;
+
+#[derive(Debug)]
+pub struct GeneralCreationValue {
+  name: String,
+  operation: GeneralOperation,
+}
+
+impl GeneralCreationValue {
+  fn new(name: String, operation: GeneralOperation) -> Self {
+    Self { name, operation }
+  }
+
+  pub fn to_name_and_operation(self) -> (String, Rc<GeneralOperation>) {
+    (self.name, Rc::new(self.operation))
+  }
+}
+
+impl YaDeserialize for GeneralCreationValue {
+  fn deserialize<R: Read>(reader: &mut Deserializer<R>) -> Result<Self, String> {
+    let inner: InnerGeneralCreationValue = from_deserializer(reader)?;
+    let (name, operation) =
+      match inner {
+        InnerGeneralCreationValue { name, value: Some(value), .. } =>
+          (name, GeneralOperation::Value(value)),
+        InnerGeneralCreationValue { name, elements, .. } if !elements.is_empty() =>
+          (name, GeneralOperation::Operation(elements)),
+        InnerGeneralCreationValue { name, .. } =>
+          (name, GeneralOperation::Empty)
+      };
+    Ok(Self::new(name, operation))
+  }
+}
+
+#[derive(Debug, YaDeserialize)]
+#[yaserde(rename = "value")]
+struct InnerGeneralCreationValue {
+  #[yaserde(attribute)]
+  name: String,
+  #[yaserde(attribute)]
+  value: Option<String>,
+  #[yaserde(rename = "creation", prefix = "general", namespace = "general: http://www.ato.net/xmlns/general")]
+  elements: Vec<InnerGeneralCreation>,
+}
