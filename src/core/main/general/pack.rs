389e138ddd2c6291ae0e8a0b3cd9bd109a3803b0
lukas <lukas.weber.4.12@gmail.com>
2022-06-05 13:32:49 +0200

    use single pack

--- src/core/main/general/pack.rs
+++ src/core/main/general/pack.rs
@@ -9,27 +9,19 @@ use crate::core::traits::namespace::Namespace;
 use crate::core::traits::pack::{Pack, ProvidePack};
 
 pub struct PackProvider {
-  path_packs: HashMap<Namespace, PathPack>,
-  string_packs: HashMap<Namespace, StringPack>,
+  packs: HashMap<Namespace, PathPack>,
 }
 
 impl PackProvider {
   pub fn from_root<P: AsRef<Path> + ?Sized>(root: &P) -> Self {
     Self {
-      path_packs: PathPack::from_root(root),
-      string_packs: StringPack::from_root(root),
+      packs: PathPack::from_root(root),
     }
   }
 }
 
 impl ProvidePack<PathPack, PathValue> for PackProvider {
   fn packs(&self) -> &HashMap<Namespace, PathPack> {
-    &self.path_packs
-  }
-}
-
-impl ProvidePack<StringPack, StringValue> for PackProvider {
-  fn packs(&self) -> &HashMap<Namespace, StringPack> {
-    &self.string_packs
+    &self.packs
   }
 }
