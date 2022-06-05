57c92a726f098f01a65a31a0d089ca0801954bc9
lukas <lukas.weber.4.12@gmail.com>
2022-06-05 13:44:58 +0200

    add Pack

--- src/core/main/general/pack.rs
+++ src/core/main/general/pack.rs
@@ -1,1 +1,11 @@
+use crate::core::main::general::operation::GeneralOperation;
+use crate::core::main::general::parameter::GeneralParameters;
+use crate::core::traits::namespace::Namespace;
+
 pub mod provider;
+
+struct Pack {
+  namespace: Namespace,
+  parameters: GeneralParameters,
+  operation: GeneralOperation,
+}
