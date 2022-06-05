97f31cba7f78b6bed8f4b23e18056c1069725566
lukas <lukas.weber.4.12@gmail.com>
2022-06-05 14:23:55 +0200

    fix parameter YaDeserialize

--- src/core/main/general/parameter.rs
+++ src/core/main/general/parameter.rs
@@ -1,16 +1,21 @@
-pub mod inner;
-
 use std::io::Read;
+
 use yaserde::__xml::name::OwnedName;
 use yaserde::__xml::reader::XmlEvent;
 use yaserde::de::Deserializer;
 use yaserde::YaDeserialize;
+
 use crate::core::main::action::parameter::ActionParameter;
 use crate::core::main::boolean::parameter::BooleanParameter;
+use crate::core::main::general::parameter::inner::InnerParameter;
+use crate::core::main::general::variant::Variant;
 use crate::core::main::number::parameter::NumberParameter;
 use crate::core::main::path::parameter::PathParameter;
 use crate::core::main::string::parameter::StringParameter;
 use crate::core::parse::from_deserializer;
+use crate::core::traits::namespace::Namespace;
+
+pub mod inner;
 
 #[derive(Debug, Default, YaDeserialize)]
 pub struct GeneralParameters {
@@ -19,28 +24,25 @@ pub struct GeneralParameters {
 }
 
 #[derive(Debug)]
-pub enum Parameter {
-  Action(ActionParameter),
-  Boolean(BooleanParameter),
-  Number(NumberParameter),
-  Path(PathParameter),
-  String(StringParameter),
+pub struct Parameter {
+  name: String,
+  namespace: Option<Namespace>,
+  variant: Variant,
 }
 
 impl Parameter {
   fn from_xml_name<R: Read>(reader: &mut Deserializer<R>, name: OwnedName) -> Result<Self, String> {
-    if let OwnedName { local_name, namespace: Some(namespace), .. } = name {
-      match (local_name.as_str(), namespace.as_str()) {
-        ("parameter", "http://www.ato.net/xmlns/action") => Ok(Parameter::Action(from_deserializer(reader)?)),
-        ("parameter", "http://www.ato.net/xmlns/boolean") => Ok(Parameter::Boolean(from_deserializer(reader)?)),
-        ("parameter", "http://www.ato.net/xmlns/number") => Ok(Parameter::Number(from_deserializer(reader)?)),
-        ("parameter", "http://www.ato.net/xmlns/path") => Ok(Parameter::Path(from_deserializer(reader)?)),
-        ("parameter", "http://www.ato.net/xmlns/string") => Ok(Parameter::String(from_deserializer(reader)?)),
-        value => Err(format!("parameter: wrong name: {:?}", value))
-      }
-    } else {
-      Err("parameter: no namespace found".to_owned())
-    }
+    let inner: InnerParameter = from_deserializer(reader)?;
+    let variant = match name.namespace {
+      Some(xml_namespace) => Variant::from_xml_namespace(&xml_namespace)?,
+      None => return Err("Parameter: no xml_namespace provided".to_owned())
+    };
+    Ok(Self::from_inner(inner, variant))
+  }
+
+  fn from_inner(inner: InnerParameter, variant: Variant) -> Self {
+    let (name, namespace) = inner.to_name_and_optional_namespace();
+    Self { name, namespace, variant }
   }
 }
 
--- src/core/main/general/variant.rs
+++ src/core/main/general/variant.rs
@@ -9,14 +9,14 @@ pub enum Variant {
 }
 
 impl Variant {
-  pub fn from_xml_namespace(xml_namespace: &str) -> Result<Self, BuildError> {
+  pub fn from_xml_namespace(xml_namespace: &str) -> Result<Self, String> {
     match xml_namespace {
       "http://www.ato.net/xmlns/action" => Ok(Self::Action),
       "http://www.ato.net/xmlns/boolean" => Ok(Self::Boolean),
       "http://www.ato.net/xmlns/number" => Ok(Self::Number),
       "http://www.ato.net/xmlns/path" => Ok(Self::Path),
       "http://www.ato.net/xmlns/string" => Ok(Self::String),
-      _ => Err(BuildError::new_unknown_xml_namespace(xml_namespace.to_owned()))
+      _ => Err(format!("UnknownXmlNamespace: '{}'", xml_namespace))
     }
   }
 }
