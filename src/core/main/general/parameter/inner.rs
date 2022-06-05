8c0599de3d8d34619cb3022d905c5c9ab53ddf83
lukas <lukas.weber.4.12@gmail.com>
2022-06-05 13:56:48 +0200

    add conversion method for inner parameter

--- src/core/main/general/parameter/inner.rs
+++ src/core/main/general/parameter/inner.rs
@@ -1,3 +1,6 @@
+use crate::core::traits::namespace;
+use crate::core::traits::namespace::Namespace;
+
 #[derive(Debug, YaDeserialize)]
 #[yaserde(rename = "parameter")]
 pub struct InnerParameter {
@@ -5,4 +8,12 @@ pub struct InnerParameter {
   name: String,
   #[yaserde(attribute)]
   namespace: Option<String>,
+}
+
+impl InnerParameter {
+  pub fn to_name_and_optional_namespace(self) -> (String, Option<Namespace>) {
+    let name = self.name;
+    let optional_namespace = self.namespace.and_then(Namespace::new);
+    (name, optional_namespace)
+  }
 }
