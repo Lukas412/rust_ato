2d07420ff7e00482ddc819bf925c7cef624895f2
lukas <lukas.weber.4.12@gmail.com>
2022-06-05 14:06:05 +0200

    add variant from xml_namespace

--- src/core/main/general/variant.rs
+++ src/core/main/general/variant.rs
@@ -1,7 +1,22 @@
+use crate::BuildError;
+
 pub enum Variant {
   Action,
   Boolean,
   Number,
   Path,
   String,
+}
+
+impl Variant {
+  pub fn from_xml_namespace(xml_namespace: &str) -> Result<Self, BuildError> {
+    match xml_namespace {
+      "http://www.ato.net/xmlns/action" => Ok(Self::Action),
+      "http://www.ato.net/xmlns/boolean" => Ok(Self::Boolean),
+      "http://www.ato.net/xmlns/number" => Ok(Self::Number),
+      "http://www.ato.net/xmlns/path" => Ok(Self::Path),
+      "http://www.ato.net/xmlns/string" => Ok(Self::String),
+      _ => Err(BuildError::new_unknown_xml_namespace(xml_namespace.to_owned()))
+    }
+  }
 }
