5ed880ec26cadcdcda5628bc46fb790c8da3584a
lukas <lukas.weber.4.12@gmail.com>
2022-06-05 17:56:01 +0200

    add value

--- src/core/main/general/value.rs
+++ src/core/main/general/value.rs
@@ -1,21 +1,38 @@
+use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
 use rust_decimal::Decimal;
 
 use crate::core::main::action::value::Action;
+use crate::core::traits::namespace::Namespace;
 
-#[derive(Debug, Clone)]
-pub enum CombinedGeneralValue {
-  None,
-  Action(Action),
-  Boolean(bool),
-  Number(Decimal),
-  Path(PathBuf),
-  String(String),
+#[derive(Debug)]
+pub struct Value<T> {
+  value: T,
+  namespace: Namespace,
 }
 
-impl Default for CombinedGeneralValue {
-  fn default() -> Self {
-    Self::None
+impl<T> Value<T>
+  where T: Default
+{
+  pub fn new(value: T, namespace: Namespace) -> Self {
+    Self { value, namespace }
+  }
+
+  pub fn default_with_namespace(namespace: Namespace) -> Self {
+    let value = T::default();
+    Self { value, namespace }
+  }
+
+  pub fn value(&self) -> &T {
+    &self.value
+  }
+}
+
+impl<T> Display for Value<T>
+  where T: Display
+{
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "Value({} from {})", self.value, self.namespace)
   }
 }
