85c4ca6f5a13b2181cd8961a0b346ec3142b02de
lukas <lukas.weber.4.12@gmail.com>
2022-06-06 23:03:50 +0200

    add value operation

--- src/core/main/general/operation.rs
+++ src/core/main/general/operation.rs
@@ -1,11 +1,13 @@
 use crate::{BuildError, CreationStack, GeneralCreation};
 use crate::core::main::general::operation::empty::build_empty;
+use crate::core::main::general::operation::value::build_value;
 use crate::core::main::general::pack::provider::PackProvider;
 use crate::core::main::general::variant::Variant;
 use crate::core::main::string::value::StringValue;
 use crate::core::traits::namespace::Namespace;
 
 pub mod empty;
+pub mod value;
 
 #[derive(Debug)]
 pub struct Operation {
@@ -17,7 +19,7 @@ impl Operation {
   fn build(&self, pack_provider: &PackProvider, stack: &mut CreationStack) -> Result<StringValue, BuildError> {
     match &self.action {
       OperationAction::Empty => build_empty(&self.variant, stack),
-      OperationAction::Value { text } => todo!(),
+      OperationAction::Value { text } => build_value(&self.variant, stack, text),
       OperationAction::GetArgument { name, namespace } => todo!(),
     }
   }
---
+++ src/core/main/general/operation/value.rs
@@ -0,0 +1,31 @@
+use std::str::FromStr;
+
+use crate::{BuildError, CreationStack};
+use crate::core::main::general::value::{Data, Value};
+use crate::core::main::general::variant::Variant;
+use crate::core::traits::namespace::{GetNamespace, Namespace};
+
+pub fn build_value(variant: &Variant, stack: &CreationStack, text: &String) -> Result<Value, BuildError> {
+  let data = get_data(text, stack.get_namespace())?;
+  let namespace = stack.get_owned_namespace();
+  Ok(Value::new(data, namespace))
+}
+
+fn get_data(text: &String, namespace: &Namespace) -> Result<Data, BuildError> {
+  let data =
+    match variant {
+      Variant::Action => Data::Action(from_text(text, namespace)?),
+      Variant::Boolean => Data::Boolean(from_text(text, namespace)?),
+      Variant::Number => Data::Number(from_text(text, namespace)?),
+      Variant::Path => Data::Path(from_text(text, namespace)?),
+      Variant::String => Data::String(from_text(text, namespace)?),
+    };
+  Ok(data)
+}
+
+fn from_text<T: FromStr>(text: &String, namespace: &Namespace) -> Result<T, BuildError> {
+  match T::from_str(text) {
+    Ok(result) => Ok(result),
+    Err(_) => BuildError::new_can_not_convert_text_to_value_error(&text, namespace.to_owned()),
+  }
+}
