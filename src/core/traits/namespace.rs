f618e48b1af050d84be6adfe380f273b3418f145
lukas <lukas.weber.4.12@gmail.com>
2022-06-06 22:44:58 +0200

    remove GetNamespace trait

--- src/core/traits/namespace.rs
+++ src/core/traits/namespace.rs
@@ -20,11 +20,3 @@ impl Display for Namespace {
     write!(f, "{}", self.0)
   }
 }
-
-pub trait GetNamespace {
-  fn get_namespace(&self) -> &Namespace;
-
-  fn get_owned_namespace(&self) -> Namespace {
-    self.get_namespace().to_owned()
-  }
-}
