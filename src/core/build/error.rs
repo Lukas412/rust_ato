499a671d305ebed9603d5082be38c3c51bf6cbc7
lukas <lukas.weber.4.12@gmail.com>
2022-06-06 22:44:11 +0200

    update error

--- src/core/build/error.rs
+++ src/core/build/error.rs
@@ -1,97 +1,51 @@
 use std::fmt::{Display, Formatter};
+
 use crate::BuildError::{OperationNotFound, UnknownXmlNamespace};
 use crate::core::build::error::BuildError::{Pack, Requirement, Value};
 use crate::core::traits::namespace::Namespace;
 
-pub enum BuildError {
-  OperationNotFound {
-    name: String,
-    namespace: Namespace,
-    backtrace: Backtrace,
-  },
-  Pack {
-    namespace: Namespace,
-    backtrace: Backtrace,
-  },
-  Requirement {
-    name: String,
-    namespace: Namespace,
-    backtrace: Backtrace,
-  },
-  UnknownXmlNamespace {
-    xml_namespace: String,
-    backtrace: Backtrace,
-  },
-  Value {
-    name: String,
-    namespace: Namespace,
-    backtrace: Backtrace,
-  },
+pub struct BuildError {
+  message: String,
+  namespace: Namespace,
+  backtrace: Backtrace,
 }
 
 impl BuildError {
-  pub fn new_operation_not_found_error(name: String, namespace: Namespace) -> Self {
-    OperationNotFound { name, namespace, backtrace: Backtrace::default() }
+  pub fn new_operation_not_found_error(name: &String, namespace: Namespace) -> Self {
+    let message = format!("OperationNotFound: {name}");
+    Self::new(message, namespace)
   }
 
-  pub fn new_pack(namespace: Namespace) -> Self {
-    Pack { namespace, backtrace: Backtrace::default() }
+  pub fn new_unknown_xml_namespace_error(xml_namespace: &String, namespace: Namespace) -> Self {
+    let message = format!("UnknownXmlNamespace: {xml_namespace}");
+    Self::new(message, namespace)
   }
 
-  pub fn new_requirement(name: String, namespace: Namespace) -> Self {
-    Requirement { name, namespace, backtrace: Backtrace::default() }
+  pub fn new_can_not_convert_text_to_value_error(text: &String, namespace: Namespace) -> Self {
+    let message = format!("CanNotConvertTextToValue: {text}");
+    Self::new(message, namespace)
   }
+}
 
-  pub fn new_unknown_xml_namespace(xml_namespace: String) -> Self {
-    UnknownXmlNamespace { xml_namespace, backtrace: Backtrace::default() }
-  }
-
-  pub fn new_value(name: String, namespace: Namespace) -> Self {
-    Value { name, namespace, backtrace: Backtrace::default() }
-  }
-
+impl BuildError {
   pub fn add_backtrace(&mut self, trace: String) {
-    self.mut_backtrace().add(trace)
+    self.backtrace.add(trace)
   }
 }
 
 impl BuildError {
-  fn message(&self) -> String {
-    match self {
-      OperationNotFound { name, namespace, .. } => format!("OperationNotFound: '{} -> {}'", namespace, name),
-      Pack { .. } => format!(""),
-      Requirement { .. } => format!(""),
-      UnknownXmlNamespace { xml_namespace, .. } => format!("UnknownXmlNamespace: '{}'", xml_namespace),
-      Value { .. } => format!(""),
-    }
-  }
-
-  fn backtrace(&self) -> &Backtrace {
-    match self {
-      OperationNotFound { backtrace, .. } => backtrace,
-      Value { backtrace, .. } => backtrace,
-      Requirement { backtrace,.. } => backtrace,
-      UnknownXmlNamespace { backtrace,.. } => backtrace,
-      Pack { backtrace,.. } => backtrace,
-    }
-  }
-
-   fn mut_backtrace(&mut self) -> &mut Backtrace {
-    match self {
-      OperationNotFound { backtrace, .. } => backtrace,
-      Value { backtrace, .. } => backtrace,
-      Requirement { backtrace,.. } => backtrace,
-      UnknownXmlNamespace { backtrace,.. } => backtrace,
-      Pack { backtrace,.. } => backtrace,
-    }
+  fn new(message: String, namespace: Namespace) -> Self {
+    let backtrace = Backtrace::default();
+    Self { message, namespace, backtrace }
   }
 }
 
 impl Display for BuildError {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    let error_message = self.message();
-    let backtrace = self.backtrace();
-    write!(f, "{}{}", backtrace, error_message)
+    let message = &self.message;
+    let namespace = &self.namespace;
+    let backtrace = &self.backtrace;
+    write!(f, "{}{} in {}", backtrace, message, namespace)
   }
 }
 
