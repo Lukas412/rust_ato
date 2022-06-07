1f30e3bbe2cdaabca114b75164ed6f81a14be180
lukas <lukas.weber.4.12@gmail.com>
2022-06-07 11:45:37 +0200

    move to core

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,1 +1,10 @@
-pub mod main;
+pub mod parameter;
+pub mod value;
+pub mod creation;
+pub mod reference;
+pub mod pack;
+pub mod operation;
+pub mod variant;
+pub mod namespace;
+pub mod error;
+pub mod parse;
