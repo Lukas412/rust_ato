fff8fea0a9fe254a9c162637e118772ce9c965df
lukas <lukas.weber.4.12@gmail.com>
2022-06-14 18:15:11 +0200

    update common lib

--- src/common/src/ends_with.rs
+++ src/common/src/ends_with.rs
@@ -1,11 +1,11 @@
 use std::path::PathBuf;
 
-trait EndsWith {
-  fn ends_with(&self, s: &str) -> bool;
+trait EndsWithStr {
+  fn ends_with_str(&self, s: &str) -> bool;
 }
 
-impl EndsWith for PathBuf {
-  fn ends_with(&self, s: &str) -> bool {
+impl EndsWithStr for PathBuf {
+  fn ends_with_str(&self, s: &str) -> bool {
     match self.to_str() {
       Some(string) => string.ends_with(s),
       None => false,
