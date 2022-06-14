ad210557033efb7e9b048a0f3c9b000237abef24
lukas <lukas.weber.4.12@gmail.com>
2022-06-14 17:22:04 +0200

    wip

--- src/core/pack.rs
+++ src/core/pack.rs
@@ -1,6 +1,12 @@
+use std::collections::HashMap;
+use std::fs::DirEntry;
+use std::iter::FromIterator;
+use std::path::Path;
+use walkdir::WalkDir;
 use crate::core::namespace::Namespace;
 use crate::core::operation::Operation;
 use crate::core::parameter::Parameters;
+use crate::from_file;
 
 pub mod provider;
 
@@ -11,7 +17,26 @@ struct Pack {
 }
 
 impl Pack {
+  pub fn all_from_root<P: AsRef<Path>>(root: &P) -> HashMap<Namespace, Self> {
+    let packs =
+      WalkDir::new(root)
+        .into_iter()
+        .filter_map(Result::ok)
+        .map(DirEntry::into_path)
+        .filter(Self::is_pack_path)
+        .map(from_file)
+        .filter_map(Result::ok)
+        .map(|pack: Self| (pack.namespace().to_owned(), pack));
+    HashMap::from_iter(packs)
+  }
+
   pub fn get_operation(&self) -> &Operation {
     &self.operation
+  }
+}
+
+impl Pack {
+  fn is_pack_path<P: AsRef<Path>>(path: &P) -> bool {
+    todo!()
   }
 }
