66ac150e477c9ce8cd1b31abd12f37636a326f87
lukas <lukas.weber.4.12@gmail.com>
2022-06-19 20:18:21 +0200

    add function peek_start_element

--- src/core/parse.rs
+++ src/core/parse.rs
@@ -1,10 +1,14 @@
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use yaserde::__xml::reader::XmlEvent;
 
+use yaserde::__xml::attribute::OwnedAttribute;
+use yaserde::__xml::name::OwnedName;
+use yaserde::__xml::namespace::Namespace;
+use yaserde::__xml::reader::XmlEvent;
 use yaserde::de::{Deserializer, from_reader};
 use yaserde::YaDeserialize;
+
 use crate::core::variant::{DeserializeWithVariant, Variant};
 
 pub fn from_file<T: YaDeserialize, P: AsRef<Path>>(file: P) -> Result<T, String> {
@@ -21,12 +25,17 @@ pub fn from_deserializer<R: Read, T: YaDeserialize>(reader: &mut Deserializer<R>
 pub fn from_deserializer_with_variant<R, T>(reader: &mut Deserializer<R>) -> Result<T, String>
   where R: Read, T: DeserializeWithVariant
 {
+  let (name, _, _) = peek_start_element(reader)?;
+  let variant = Variant::from_owned_name(name)?;
+  let inner: T::Inner = from_deserializer(reader)?;
+  T::from_inner(inner, variant)
+}
+
+pub fn peek_start_element<R: Read>(reader: &mut Deserializer<R>) -> Result<(&OwnedName, &Namespace, &Vec<OwnedAttribute>), String> {
   let peek = reader.peek()?;
-  if let XmlEvent::StartElement { name, .. } = peek {
-    let variant = Variant::from_owned_name(name)?;
-    let inner: T::Inner = from_deserializer(reader)?;
-    T::from_inner(inner, variant)
-  } else {
-    Err(format!("ExpectStartElement: {:?}", peek))
+  match peek {
+    XmlEvent::StartElement { name, namespace, attributes } =>
+      Ok((name, namespace, attributes)),
+    _ => Err(format!("Expect StartElement: {:?}", peek))
   }
 }
