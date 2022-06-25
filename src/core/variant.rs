94d873797a316dfa0fa112b95a88ddcbb5de09ea
lukas <lukas.weber.4.12@gmail.com>
2022-06-25 12:36:22 +0200

    impl Display for Variant

--- src/core/variant.rs
+++ src/core/variant.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use yaserde::__xml::name::OwnedName;
 use yaserde::YaDeserialize;
 
@@ -14,6 +15,19 @@ pub enum Variant {
 impl Default for Variant {
   fn default() -> Self {
     Self::None
+  }
+}
+
+impl Display for Variant {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Variant::None => write!(f, "None"),
+      Variant::Action => write!(f, "Action"),
+      Variant::Boolean => write!(f, "Boolean"),
+      Variant::Number => write!(f, "Number"),
+      Variant::Path => write!(f, "Path"),
+      Variant::String => write!(f, "String")
+    }
   }
 }
 
