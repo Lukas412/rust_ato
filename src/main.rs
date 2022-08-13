a70931e403576347e5e4c853a47d6494a608ad7f
lukas <lukas.weber.4.12@gmail.com>
2022-08-13 02:59:38 +0200

    fix fmt::Debug trait error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+extern crate rust_decimal;
+extern crate walkdir;
 extern crate yaserde;
 #[macro_use]
 extern crate yaserde_derive;
-extern crate walkdir;
-extern crate rust_decimal;
 
-use std::rc::Rc;
+use core::build::Build;
 use core::builder::Builder;
+use core::error::BuildError;
 
 use crate::core::creation::Creation;
 use crate::core::creation::stack::CreationStack;
@@ -16,10 +17,15 @@ mod helpers;
 
 fn main() {
   let builder = Builder::from_root("src/bundles").unwrap();
-  let mut build = builder.create_build("src/creations/test2.creation.xml").unwrap();
+
+  let mut build = match builder.create_build("src/creations/test2.creation.xml") {
+    Ok(build) => build,
+    Err(error) => panic!(format!("Error while creating build: {}", error))
+  };
+
   let value = build.build();
   match value {
-    Ok(value) => println!("{:?}", value),
-    Err(error) => println!("{}", error)
+    Ok(value) => println!("{}", value),
+    Err(error) => println!("Error while building: {}", error)
   }
 }
