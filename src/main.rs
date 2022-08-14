0e5dc0298e5add912996289efa249f8b9de72983
lukas <lukas.weber.4.12@gmail.com>
2022-08-14 20:00:22 +0200

    improve error output

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate yaserde;
 #[macro_use]
 extern crate yaserde_derive;
 
+use error_stack::ResultExt;
 use crate::core::build::Build;
 use crate::core::builder::Builder;
 
@@ -22,12 +23,12 @@ fn main() {
 
   let mut build = match builder.create_build("src/creations/test2.creation.xml") {
     Ok(build) => build,
-    Err(error) => panic!("Error while creating build: {}", error)
+    Err(error) => panic!("\n{:?}", error)
   };
 
   let value = build.build();
   match value {
     Ok(value) => println!("{}", value),
-    Err(error) => panic!("Error while building: {}", error)
+    Err(error) => panic!("\n{:?}", error)
   }
 }
