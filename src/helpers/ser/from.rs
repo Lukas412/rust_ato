0997ffb7ab9176e5b661cfd458a72c9d9f2e9edd
lukas <lukas.weber.4.12@gmail.com>
2022-08-14 02:47:08 +0200

    fix errors in helpers::ser::from

--- src/helpers/ser/from.rs
+++ src/helpers/ser/from.rs
@@ -1,26 +1,21 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
+use std::ops::Deref;
+use std::path::{Path, PathBuf};
 
-use error_stack::{IntoReport, ResultExt};
+use error_stack::{Context, IntoReport, ResultExt};
 use yaserde::de::{Deserializer, from_reader};
 use yaserde::YaDeserialize;
+use crate::errors::ser::from_file::DeserializeFromFileError;
+use crate::errors::ser::string::to_string_error_context;
 
-struct DeserializeFromFileError<'a, P> {
-  file_path: &'a P,
-}
-
-impl<'a, P: AsRef<Path>> DeserializeFromFileError<'a, P> {
-  fn new(file_path: &'a P) -> Self {
-    Self { file_path }
-  }
-}
-
-pub(crate) fn from_file<T: YaDeserialize, P: AsRef<Path> + ?Sized>(file_path: &P) -> error_stack::Result<T, DeserializeFromFileError<P>> {
+pub(crate) fn from_file<T: YaDeserialize, P: AsRef<Path> + Debug + Sized>(file_path: &P) -> error_stack::Result<T, DeserializeFromFileError> {
   let file = File::open(file_path)
     .report().change_context(DeserializeFromFileError::new(file_path))?;
-  from_reader(file)
-    .report().change_context(DeserializeFromFileError::new(file_path))
+  let deserialize_result = from_reader(file);
+  to_string_error_context(deserialize_result).change_context(DeserializeFromFileError::new(file_path))
 }
 
 pub(crate) fn from_deserializer<R: Read, T: YaDeserialize>(reader: &mut Deserializer<R>) -> Result<T, String> {
